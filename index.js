c51cef7ef07c1ffa73f52576cf9b04c1fb7183a6
nyawiradev <njaginjoyce@gmail.com>
2022-06-08 21:52:51 +0300

    commit tree b66dd956bc61cbf3c7a3347c0a5691ce9fed26f4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const writeCards = (name) => {
+    let namesArr = [];
+    for(let i = 0; i < name.length; i++) {
+       namesArr.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return namesArr;
+ }
+ const countDown = (num) => {
+    for(let i = num; i >= 0; i--) {
+       console.log(i);
+    }
+ }
+ 
